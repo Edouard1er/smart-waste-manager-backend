9e3e15874601643b927a0bb235a2abb50c7f6bc7
Edouard Chevenslove <edouardchevenslove98@gmail.com>
2023-12-21 00:37:37 +0100

    Best routes

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
 from redis import Redis
+from flask import Flask, render_template, request, jsonify
+import requests
 
 app = Flask(__name__)
 redis = Redis(host='localhost', port=6379)
@@ -8,6 +9,51 @@ redis = Redis(host='localhost', port=6379)
 def testwithredis():
     redis.incr('hits')
     return 'Nombre de visites : {}'.format(redis.get('hits').decode('utf-8'))
+
+GOOGLE_MAPS_API_KEY = ''
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/get_directions', methods=['GET'])
+def get_directions():
+    try:
+        
+        origin = "40.748817,-73.985428"  
+
+        
+        destination = "34.052235,-118.243683"
+
+        api_url = 'https://maps.googleapis.com/maps/api/directions/json'
+        params = {
+            'origin': origin,
+            'destination': destination,
+            'key': GOOGLE_MAPS_API_KEY,
+        }
+
+        response = requests.get(api_url, params=params)
+        data = response.json()
+
+        routes = data.get('routes', [])
+        if routes:
+            route = routes[0]
+            distance = route.get('legs')[0].get('distance').get('text')
+            duration = route.get('legs')[0].get('duration').get('text')
+            steps = route.get('legs')[0].get('steps')
+
+            # Retourner les informations sous forme de JSON
+            return jsonify({
+                'success': True,
+                'distance': distance,
+                'duration': duration,
+                'steps': steps
+            })
+        else:
+            return jsonify({'success': False, 'error': 'Aucun itinéraire trouvé'})
+
+    except Exception as e:
+        return jsonify({'success': False, 'error': str(e)})
 
 if __name__ == '__main__':
     app.run(debug=True)
