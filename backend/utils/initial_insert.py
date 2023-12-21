d113d231642cb2e4f6056dfac43f972eb5e5c247
silvanshine <theo.piacentini@gmail.com>
2023-12-21 17:51:24 +0100

    start test for model

--- backend/utils/initial_insert.py
+++ backend/utils/initial_insert.py
@@ -1,9 +1,9 @@
 """insère les données initiales données dans le sujet"""
 
-from pathlib import Path
 
 import pandas as pd
 from icecream import colorize, ic
+from pathlib import Path
 
 from backend.Database import Database
 
@@ -16,47 +16,49 @@ def main():
     )  # print debug
 
     db = Database()
-
-    # df = pd.DataFrame()
-    # ic()
-
-    # print(df)
-    excel_sheet_names, df = get_initial_data()
-
-    for key, value in df.items():
-        name_zone = excel_sheet_names[key]
-        # key_zone = Database.add_data_zone(nom=name_zone)
-
-        # key_poubelle = Database.add_data_poubelle
-
-        for row in value.iloc[:, 2:].iterrows():
-            row = row[1]
-
-            for i in range(0, len(row), 3):
-                coeff_touriste = row[i + 2]
-                remplissage = row[i + 1]
-                ic(coeff_touriste)
-                ic(remplissage)
-
-            # db.add_data_poubelle(id_zone=key_zone)
-            # db.add_data_historique_poubelle
-
-
-def get_initial_data():
     path_to_root = Path(__file__) / ".." / ".." / ".."
-    path_to_root = path_to_root.resolve()
+    path_to_root= path_to_root.resolve()
+    path_to_excel = path_to_root / "data" / "taux_remplissage_comcom_nebbiu.xlsx"
 
-    path_to_data = path_to_root / "data"
+    df = init_excel_to_df(str(path_to_excel))
+    
+    ic(df)
 
-    excel_sheet_names = (
-        pd.ExcelFile(path_to_data / "taux_remplissage_comcom_nebbiu.xlsx")
-    ).sheet_names
-    df = pd.read_excel(
-        path_to_data / "taux_remplissage_comcom_nebbiu.xlsx", sheet_name=list(range(16))
+
+
+def init_excel_to_df(path="../../data/taux_remplissage_comcom_nebbiu.xlsx"):
+    excel_sheet_names = (pd.ExcelFile(path)).sheet_names
+    df_sheets = pd.read_excel(path, sheet_name=excel_sheet_names, header=1)
+
+    list_result = []
+
+    for key, value in df_sheets.items():
+        zone = key
+        list_gps = []
+        for i, row in value.iterrows():
+            flagGPS = False
+            if i == 0:
+                flagGPS = True
+
+            date = row.iloc[1]
+            for j in range(2, len(row), 3):
+                num_poubelle = int((j - 2) / 3)
+                poubelle_nom = str(key) + "-poubelle-" + str(num_poubelle)
+                if flagGPS:
+                    list_gps.append(row.iloc[j])
+                gps = list_gps[num_poubelle]
+                remplissage = row.iloc[j + 1]
+                coeff_touriste = row.iloc[j + 2]
+
+                list_result.append(
+                    [poubelle_nom, zone, coeff_touriste, gps, date, remplissage]
+                )
+
+    df_result = pd.DataFrame(
+        list_result,
+        columns=["nom", "zone", "coeff_touriste", "gps", "date", "remplissage"],
     )
-
-    return excel_sheet_names, df
-
+    return df_result
 
 if __name__ == "__main__":
     main()
