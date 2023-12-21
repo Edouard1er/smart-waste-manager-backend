3cdd359d767c9e7e48a7080c7a21cd87035efef1
silvanshine <theo.piacentini@gmail.com>
2023-12-21 11:40:00 +0100

    import data from hackaton subject in pandas dataframe

--- backend/models/connection_db.py
+++ backend/models/connection_db.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+
+import pandas as pd
 from dotenv import dotenv_values
 from icecream import colorize, ic
 from pymongo import MongoClient
@@ -11,6 +14,17 @@ def main():
     )  # print debug
 
     db = get_database(NAME_DB)
+
+    path_to_root = Path(__file__) / ".." / ".." / ".."
+    path_to_root = path_to_root.resolve()
+    
+    path_to_data = path_to_root / "data"
+    
+    ic(path_to_data)
+    
+    df_poubelles = pd.read_excel(path_to_data/ "taux_remplissage_comcom_nebbiu.xlsx")
+    ic(df_poubelles)
+    
 
 
 def get_database(db_name):
@@ -34,9 +48,9 @@ def get_database(db_name):
 
     try:
         client.admin.command("ismaster")
-        print("connexion to db established !")
+        print("connexion to db established !\n")
     except ConnectionError:
-        print("Server not available")
+        print("Server not available\n")
 
     # Create the database for our example (we will use the same database throughout the tutorial)
     return client[db_name]
